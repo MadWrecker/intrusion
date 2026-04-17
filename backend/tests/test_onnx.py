a53f193e739719d75e2ad489f260828c07358ab2
M8onkey <mithulmanu13@gmail.com>
2026-04-18 04:41:49 +0530

    first commit

--- backend/tests/test_onnx.py
+++
@@ -1,4 +0,0 @@
-import onnxruntime as ort
-session = ort.InferenceSession("models/weights/arcface.onnx", providers=['CPUExecutionProvider'])
-for i in session.get_inputs():
-    print(f"Input Name: {i.name}, Shape: {i.shape}, Type: {i.type}")
